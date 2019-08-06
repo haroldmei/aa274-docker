71264ee63ea283645a21863691d32ba3fff84e1c
Toki Migimatsu <toki.migimatsu@gmail.com>
2019-08-06 14:08:39 -0700

    Add cuda install

--- install_docker.sh
+++ install_docker.sh
@@ -16,6 +16,20 @@
 # # Install the latest version of Docker CE and containerd
 # sudo apt-get install docker-ce docker-ce-cli containerd.io
 
+# Install CUDA
+#!/bin/bash
+echo "Checking for CUDA and installing."
+# Check for CUDA and try to install.
+if ! dpkg-query -W cuda-10-0; then
+	curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+	dpkg -i ./cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+	apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+	apt-get update
+	apt-get install cuda-10-0 -y
+fi
+# Enable persistence mode
+nvidia-smi -pm 1
+
 # Install Docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
