899b9cdd9101408ffc5edba69c70d574480ab9a8
Toki Migimatsu <toki.migimatsu@gmail.com>
2019-09-28 16:10:40 -0700

    Remove init catkin_ws

--- init_catkin_ws.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p .ros
-mkdir -p catkin_ws/src
-cd catkin_ws/src
-git clone https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
-git clone https://github.com/ROBOTIS-GIT/turtlebot3.git
-git clone https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git
-git clone https://github.com/StanfordASL/asl_turtlebot.git
